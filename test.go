885642eb64b6871e638140fd4216927c4ce1fe13
root <root@vscode-server001.australia-southeast2-a.c.algebraic-craft-321405.internal>
2021-09-18 15:57:58 +0000

    adding nmap tcp service default check

--- test.go
+++ test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 )
@@ -23,28 +24,41 @@ func Readln(r *bufio.Reader) (string, error) {
 func main() {
 	fmt.Println("hello world!")
 
-	fi := "nmap-services.csv"
+	fi := "nmap-services_tcp.csv"
 
-	f, err := os.Open(fi)
+	fileBytes, err := ioutil.ReadFile(fi)
 	if err != nil {
-		fmt.Printf("error opening file: %v\n", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	r := bufio.NewReader(f)
-	s, e := Readln(r)
-	for e == nil {
-		fmt.Println(s)
-		s, e = Readln(r)
 
-		split := strings.Fields(s)
-		fmt.Println("length: ", len(split))
-		fmt.Println(split[0])
-		fmt.Println(split[1])
-		if "1/udp" == split[1] {
-			fmt.Println(split[0])
+	sliceData := strings.Split(string(fileBytes), "\n")
+	fmt.Println(sliceData[8354])
+	fmt.Println(len(sliceData))
+
+	/*
+		f, err := os.Open(fi)
+		if err != nil {
+			fmt.Printf("error opening file: %v\n", err)
+			os.Exit(1)
 		}
+		defer f.Close()
 
-		break
-	}
+		r := bufio.NewReader(f)
+		s, e := Readln(r)
+		for e == nil {
+			fmt.Println(s)
+			s, e = Readln(r)
 
+			split := strings.Fields(s)
+			fmt.Println("length: ", len(split))
+			fmt.Println(split[0])
+			fmt.Println(split[1])
+			if "1/udp" == split[1] {
+				fmt.Println(split[0])
+			}
+
+			break
+		}
+	*/
 }
